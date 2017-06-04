2ece326a026217e2ae366bfdb000c8de2e60aa5e
Wayne Hill <waynedhill@live.com>
2017-06-03 22:31:05 -0400

    Initial Commit

---
+++ Save-My-Passwords/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  Save-My-Passwords
+//
+//  Created by Wayne Hill on 6/3/17.
+//  Copyright © 2017 Surfside Software Solutions. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+  @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+  func configureView() {
+    // Update the user interface for the detail item.
+    if let detail = self.detailItem {
+        if let label = self.detailDescriptionLabel {
+            label.text = detail.timestamp!.description
+        }
+    }
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    self.configureView()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  var detailItem: Event? {
+    didSet {
+        // Update the view.
+        self.configureView()
+    }
+  }
+
+
+}
+
